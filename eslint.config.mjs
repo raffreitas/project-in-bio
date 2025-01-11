6e6add3bdb571c6b17b60c85508f81a592c71dc7
Rafael Freitas <raffreitasx@outlook.com>
2025-01-11 10:25:35 -0300

    fix: correct import statement formatting in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -13,7 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  eslintPluginPrettierRecommended
+  eslintPluginPrettierRecommended,
 ];
 
 export default eslintConfig;
